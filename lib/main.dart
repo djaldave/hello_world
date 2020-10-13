895e1d15e8ac0b3802737e93af389bb84136ebc4
djaldave <djaldave@gmail.com>
2020-10-14 07:45:47 +0800

    displaying content on screen

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+//import a helper library
+/*
+from fluttr to get content on the screen
+*/
+import 'package:flutter/material.dart';
+
+//define a "main" function to run when our app starts
+void main() {
+//create a new text widget to show some text on the screen
+  var app = MaterialApp(
+    home: Text("Al Dave"),
+  );
+//take that widget and get it on the screen
+  runApp(app);
+}
