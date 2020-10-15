1f880f31eb3df5174024cbdd7eed024992a76221
djaldave <djaldave@gmail.com>
2020-10-15 13:09:55 +0800

    final keyword

--- lib/widgets/image_list.dart
+++ lib/widgets/image_list.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 import '../models/image_model.dart';
 
 class ImageList extends StatelessWidget {
-  List<ImageModel> images;
+  final List<ImageModel> images;
   ImageList(this.images);
 }
