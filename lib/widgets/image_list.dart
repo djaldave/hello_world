2fa6130103e8b3e7bc4f912784b5f6c9276b605d
djaldave <djaldave@gmail.com>
2020-10-15 13:17:29 +0800

    building lists with listview

--- lib/widgets/image_list.dart
+++ lib/widgets/image_list.dart
@@ -4,4 +4,12 @@ import '../models/image_model.dart';
 class ImageList extends StatelessWidget {
   final List<ImageModel> images;
   ImageList(this.images);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: images.length,
+      itemBuilder: (context, int index) {},
+    );
+  }
 }
